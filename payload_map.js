b2a340986e6688760b4e965a94576a50598b2568
Mugiwara <37186888+ciss84@users.noreply.github.com>
2024-01-03 18:47:02 +0100

    Add files via upload

--- payload_map.js
+++ payload_map.js
@@ -2,11 +2,11 @@ const payload_map =
     [
         {
             displayTitle: 'K-Stuff',
-            description: 'FW 4.03 & 4.50 Only. FPKG enabler',
-            fileName: 'ps5-kstuff-v1.1.bin',
+            description: 'FW 3.00 & FW 3.10 & FW 3.20 & FW 3.21 & FW 4.03 & 4.50 Only. FPKG enabler',
+            fileName: 'ps5-kstuff-v1.3.bin',
             author: 'sleirsgoevy',
             source: 'https://github.com/sleirsgoevy/ps4jb2/blob/ps5-403/ps5-kstuff.bin',
-            version: '1.1'
+            version: '1.3'
         },
         {
             displayTitle: 'libhijacker game-patch',
